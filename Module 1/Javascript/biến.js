a71019704324f1cab5ee5d488cce252f27f9961d
Huu Thang <nguyenhuuthang.020197@gmail.com>
2023-03-15 15:48:44 +0700

    biến

---
+++ Module 1/Javascript/biến.js
@@ -0,0 +1,28 @@
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà nội'
+
+document.write('i =' + i)
+document.write('<br/>') 
+document.write('f =' + f)
+document.write ('<br/>') 
+document.write('b =' + b)
+document.write ('<br/>') 
+document.write('s =' + s)
+document.write ('<br/>') 
+
+let width = 20
+let height = 10
+let area = width * height
+document.write('Area = ' + area)
+document.write ('<br/>') 
+
+var = prompt('nhập số a');
+var = prompt('nhập số b');
+if( a % b== 0){
+    alert(a +"bội số của" + b);
+}
+    else{
+    alert(a +"không phải bội số của"+ b);
+}
