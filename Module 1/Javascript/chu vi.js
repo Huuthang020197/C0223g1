d154fd1ea4fba94b4476c522b946e19e0ddf0f40
Huu Thang <nguyenhuuthang.020197@gmail.com>
2023-03-16 11:54:32 +0700

    chuvi

---
+++ Module 1/Javascript/chu vi.js
@@ -0,0 +1,3 @@
+let R = parseFloat(prompt("nhập R bán kính hình tròn: "));
+let C = (R * 2)* Math.PI ;
+alert("chu vi của C là : " +C )
---
+++ Module 1/Javascript/tính diện tích.js
@@ -0,0 +1,5 @@
+
+    var R = parseFloat( prompt ("nhập R bán kính hình tròn: "));
+    let S =Math.PI * R * R;
+    alert("diện tích của S là : " +S)
+
